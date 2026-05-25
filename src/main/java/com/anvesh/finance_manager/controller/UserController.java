dde630d01ba03dce7648196e6266974a63330404
Anvesh Mahajan <anveshmahajan777@gmail.com>
2026-05-26 02:31:29 +0530

    Fixed user routes

--- src/main/java/com/anvesh/finance_manager/controller/UserController.java
+++ src/main/java/com/anvesh/finance_manager/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.anvesh.finance_manager.controller;
 
+import com.anvesh.finance_manager.dto.LoginRequestDTO;
+import com.anvesh.finance_manager.dto.LoginResponseDTO;
 import com.anvesh.finance_manager.dto.UserResponseDTO;
 import com.anvesh.finance_manager.entity.User;
 import com.anvesh.finance_manager.service.UserService;
-import com.anvesh.finance_manager.dto.LoginRequestDTO;
-import com.anvesh.finance_manager.dto.LoginResponseDTO;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,21 +20,32 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public User registerUser(@RequestBody User user) {
+    public User registerUser(
+            @RequestBody User user
+    ) {
+
         return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public LoginResponseDTO login(
+            @RequestBody LoginRequestDTO request
+    ) {
+
+        return userService.login(request);
     }
 
     @GetMapping
     public List<UserResponseDTO> getAllUsers() {
+
         return userService.getAllUsers();
     }
 
     @GetMapping("/{id}")
-    public UserResponseDTO getUserById(@PathVariable Long id) {
+    public UserResponseDTO getUserById(
+            @PathVariable Long id
+    ) {
+
         return userService.getUserById(id);
-    }
-    @PostMapping("/login")
-    public LoginResponseDTO login(@RequestBody LoginRequestDTO request) {
-        return userService.login(request);
     }
 }
